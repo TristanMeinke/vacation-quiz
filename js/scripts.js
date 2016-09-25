b3d4360b6c87320afc62ba56e7ea6c5ba7d1af3b
TristanMeinke <tristan.meinke@gmail.com>
2016-09-24 17:07:19 -0700

    Second commit.  Javascript and jQuery.
    JavaScript and jQuery basics have been added to the project.  A rudimentary selection algorithm has been applied in order to choose the user's destination.  Another question has been added, which implemented a picture button.  This button type has an input that activates the submit code, however.  Further development will include error fixes, and the disabling of all buttons in a question after an answer has been selected, as well as the submit button (to prevent stacking up destinations).

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,62 @@
-  var denmarkCounter = 0;
-  var hawaiiCounter = 0;
-  var indiaCounter = 0;
-  var canadaCounter = 0;
-  // Japan is the 'e' case.
+var denmarkCounter = 0;
+var hawaiiCounter = 0;
+var indiaCounter = 0;
+var canadaCounter = 0;
+// Japan is the 'e' case.
 
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+//jQuery follows.
+$(document).ready(function() {
+
+  function displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter) {
+
+    $("#results").show();
+
+    if (denmarkCounter > hawaiiCounter && denmarkCounter > indiaCounter && denmarkCounter > canadaCounter) {
+      $("#results-denmark").show();
+
+      event.preventDefault();
+    }
+    else if (hawaiiCounter > denmarkCounter && hawaiiCounter > indiaCounter && hawaiiCounter > canadaCounter) {
+      $("#results-hawaii").show();
+      event.preventDefault();
+    }
+    else if (indiaCounter > denmarkCounter && indiaCounter > hawaiiCounter && indiaCounter > canadaCounter) {
+      $("#results-india").show();
+      event.preventDefault();
+    }
+    else if (canadaCounter > denmarkCounter && canadaCounter > hawaiiCounter && canadaCounter > indiaCounter) {
+      $("#results-canada").show();
+      event.preventDefault();
+    }
+    else {
+      $("#results-japan").show();
+      event.preventDefault();
+    }
+  }
+
+  // The following block of code increments the counters for each destination
+  // when their corresponding objects are clicked upon.
+
+  $(".denmark").click(function() {
+    denmarkCounter++;
+  });
+  $(".hawaii").click(function() {
+    hawaiiCounter++;
+  });
+  $(".india").click(function() {
+    indiaCounter++;
+  });
+  $(".canada").click(function() {
+    canadaCounter++;
+  });
+
+   $("#vacation-quiz form").submit(function(event) {
+      // The following line calls the fuction to calculate and display results,
+      // and passes it the variables it needs.
+      displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter);
+  });
+
+
+});
