58d81991345829f50e612d88716daa396359b120
TristanMeinke <tristan.meinke@gmail.com>
2016-09-25 16:36:01 -0700

    Final upload of rebuild.
    Button disabling not implemented.  Otherwise tested functional in Chrome.  Algorithm works for all cases, and show/hide is successful.

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,11 @@ var canadaCounter = 0;
 
 // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-//jQuery follows.
+// jQuery follows.
 $(document).ready(function() {
 
+  // This function defines and handles the algorithm which sorts the users answers
+  // and displays the destination they should travel to accordingly.
   function displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter) {
 
     $("#results").show();
@@ -47,7 +49,36 @@ $(document).ready(function() {
       $("#results-japan").show();
       event.preventDefault();
     }
-  }
+  };
+
+  // The following code defines and calls the functions which will disable
+  // buttons which have been used or are now ineligible.
+
+  // QUESTION 1
+
+  // function disableButtons1() {
+  // $("#canadian-lake").addClass(disabled);
+  // $("#hawaiian-sunset").addClass(disabled);
+  // $("#golden-temple").addClass(disabled);
+  // $("#danish-castle").addClass(disabled);
+  // };
+
+  // $("#canadian-lake #hawaiian-sunset #golden-temple #danish-castle").click(function() {
+  // disableButtons1();
+  // });
+
+  // QUESTION 2
+
+  // function disableButtons2() {
+  //   $("#hot").addClass(".disabled");
+  //   $("#warm").addClass(".disabled");
+  //   $("#cool").addClass(".disabled");
+  //   $("#cold").addClass(".disabled");
+  // };
+
+  // $("#hot #warm #cool #cold").click(function() {
+  //   disableButtons2();
+  // });
 
   // The following block of code increments the counters for each destination
   // when their corresponding objects are clicked upon.
@@ -65,10 +96,10 @@ $(document).ready(function() {
     canadaCounter++;
   });
 
-   $("#vacation-quiz form").submit(function(event) {
+  $("#vacation-quiz form").submit(function(event) {
       // The following line calls the fuction to calculate and display results,
       // and passes it the variables it needs.
-      displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter);
+    displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter);
   });
 
 
