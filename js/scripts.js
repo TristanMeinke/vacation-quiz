2740d5a34b81054e358489177e99f760735cf48f
TristanMeinke <Tristan@MacBook-Air-3.local>
2016-10-01 22:52:56 -0700

    Comments
    Cleaned up project files by removing commented code and adding
    additional comments for clarity.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ var denmarkCounter = 0;
 var hawaiiCounter = 0;
 var indiaCounter = 0;
 var canadaCounter = 0;
-// Japan is the 'e' case.
+// Japan is the 'else' case.
 
 // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-
 // jQuery follows.
+
 $(document).ready(function() {
 
   // This function defines and handles the algorithm which sorts the users answers
@@ -51,35 +51,6 @@ $(document).ready(function() {
     }
   };
 
-  // The following code defines and calls the functions which will disable
-  // buttons which have been used or are now ineligible.
-
-  // QUESTION 1
-
-  // function disableButtons1() {
-  // $("#canadian-lake").addClass(disabled);
-  // $("#hawaiian-sunset").addClass(disabled);
-  // $("#golden-temple").addClass(disabled);
-  // $("#danish-castle").addClass(disabled);
-  // };
-
-  // $("#canadian-lake #hawaiian-sunset #golden-temple #danish-castle").click(function() {
-  // disableButtons1();
-  // });
-
-  // QUESTION 2
-
-  // function disableButtons2() {
-  //   $("#hot").addClass(".disabled");
-  //   $("#warm").addClass(".disabled");
-  //   $("#cool").addClass(".disabled");
-  //   $("#cold").addClass(".disabled");
-  // };
-
-  // $("#hot #warm #cool #cold").click(function() {
-  //   disableButtons2();
-  // });
-
   // The following block of code increments the counters for each destination
   // when their corresponding objects are clicked upon.
 
@@ -95,6 +66,9 @@ $(document).ready(function() {
   $(".canada").click(function() {
     canadaCounter++;
   });
+
+
+  //The following code instructs the individual questions to hide themselves after they have been answered.  This section could probably benefit from being refactored with a loop of some sort.
 
 
   $("#question1").click(function() {
@@ -129,6 +103,7 @@ $(document).ready(function() {
     $("#question8").hide();
   });
 
+  //Below is the code for the submit buttons.
 
   $("#vacation-quiz form").submit(function(event) {
       // The following line calls the fuction to calculate and display results,
