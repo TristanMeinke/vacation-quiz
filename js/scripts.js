feacb3bf0d90faf20bb0e704a641f43c39607ef9
TristanMeinke <Tristan@MacBook-Air-3.local>
2016-10-01 22:38:24 -0700

    User-info form
    Added a user information form, and the show/hide functions under
    various circumstances.  This solves the problem of multiple clicks and
    submissions.

--- js/scripts.js
+++ js/scripts.js
@@ -15,36 +15,36 @@ $(document).ready(function() {
 
     $("#results").show();
 
-    if (denmarkCounter > hawaiiCounter && denmarkCounter > indiaCounter && denmarkCounter > canadaCounter) 
+    if (denmarkCounter > hawaiiCounter && denmarkCounter > indiaCounter && denmarkCounter > canadaCounter)
     {
       $("#results-denmark").show();
 
       event.preventDefault();
     }
-    else if (hawaiiCounter > denmarkCounter && hawaiiCounter > indiaCounter && hawaiiCounter > canadaCounter) 
+    else if (hawaiiCounter > denmarkCounter && hawaiiCounter > indiaCounter && hawaiiCounter > canadaCounter)
     {
       $("#results-hawaii").show();
       event.preventDefault();
     }
-    else if (indiaCounter > denmarkCounter && indiaCounter > hawaiiCounter && indiaCounter > canadaCounter) 
+    else if (indiaCounter > denmarkCounter && indiaCounter > hawaiiCounter && indiaCounter > canadaCounter)
     {
       $("#results-india").show();
       event.preventDefault();
     }
-    else if (canadaCounter > denmarkCounter && canadaCounter > hawaiiCounter && canadaCounter > indiaCounter) 
+    else if (canadaCounter > denmarkCounter && canadaCounter > hawaiiCounter && canadaCounter > indiaCounter)
     {
       $("#results-canada").show();
       event.preventDefault();
     }
-    else if (canadaCounter === denmarkCounter || canadaCounter === hawaiiCounter || canadaCounter === indiaCounter 
-            || denmarkCounter === hawaiiCounter || denmarkCounter === indiaCounter 
-            || hawaiiCounter === indiaCounter) 
+    else if (canadaCounter === denmarkCounter || canadaCounter === hawaiiCounter || canadaCounter === indiaCounter
+            || denmarkCounter === hawaiiCounter || denmarkCounter === indiaCounter
+            || hawaiiCounter === indiaCounter)
     {
       // This condition is to eliminate ties.
       $("#results-japan").show();
       event.preventDefault();
     }
-    else 
+    else
     {
       $("#results-japan").show();
       event.preventDefault();
@@ -96,10 +96,51 @@ $(document).ready(function() {
     canadaCounter++;
   });
 
+
+  $("#question1").click(function() {
+    $("#question1").hide();
+  });
+
+  $("#question2").click(function() {
+    $("#question2").hide();
+  });
+
+  $("#question3").click(function() {
+    $("#question3").hide();
+  });
+
+  $("#question4").click(function() {
+    $("#question4").hide();
+  });
+
+  $("#question5").click(function() {
+    $("#question5").hide();
+  });
+
+  $("#question6").click(function() {
+    $("#question6").hide();
+  });
+
+  $("#question7").click(function() {
+    $("#question7").hide();
+  });
+
+  $("#question8").click(function() {
+    $("#question8").hide();
+  });
+
+
   $("#vacation-quiz form").submit(function(event) {
       // The following line calls the fuction to calculate and display results,
       // and passes it the variables it needs.
     displayResults(hawaiiCounter, indiaCounter, denmarkCounter, canadaCounter);
+    $("#quiz-submission").hide();
+  });
+
+  $("#user-info form").submit(function(event) {
+    $("#user-info").hide();
+    $("#vacation-quiz").show();
+    event.preventDefault();
   });
 
 
